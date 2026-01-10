3a8faff0b49bf4fb4cffdf4b1f2da8268f53ca99
Rıdvan Güneş <gunes0070@hotmail.com>
2026-01-10 23:40:32 +0300

    feat: landing Features section design

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/landing/Hero"
 import SocialProof from "@/components/landing/SocialProof"
 import PopularCategories from "@/components/landing/PopularCategories"
+import Features from "@/components/landing/Features"
 
 const page = () => {
     return (
@@ -8,6 +9,7 @@ const page = () => {
             <Hero />
             <SocialProof />
             <PopularCategories />
+            <Features />
         </div>
     )
 }
